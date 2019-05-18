fbad7efa41ec95a51d57ddc227585ee0dbf4a9d9
wickdninja <nross63@gmail.com>
2019-05-18 11:21:40 -0400

    add cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,21 @@
+import * as Cors from 'cors';
+const cors = Cors({
+  origin: true
+});
+
 import * as functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+  cors(request, response, () => {
+    response.send('Hello from Firebase!');
+  });
 });
 
 export const upload = functions.https.onRequest((request, response) => {
-  response.send('success');
+  cors(request, response, () => {
+    response.send('success');
+  });
 });
